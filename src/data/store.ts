a14f839a6c0adb6bee7346788caf4b33eae4a90c
sebastian-mrozek <sebastian.mrozek@gmail.com>
2021-04-25 13:02:38 +1200

    Hide impl details
    Extract inner function to named functions.

--- src/data/store.ts
+++ src/data/store.ts
@@ -1,38 +1,39 @@
-import { EMPTY_LIST, TodoItem, TodoList } from '../model/model';
-import { writable, Writable, Readable, derived } from 'svelte/store';
-
-// export const todoStore: Writable<TodoList> = writable(EMPTY_LIST);
-function _toggleItem(items: TodoItem[], caption: string): TodoItem[] {
-	return items.map((item) => {
-		if (item.caption === caption) {
-			return {
-				...item,
-				completed: !item.completed
-			};
-		} else {
-			return item;
-		}
-	});
-}
+import { EMPTY_LIST, TodoItem } from '../model/model';
+import { writable, Readable, derived } from 'svelte/store';
 
 export const todoStore = (() => {
 	const { subscribe, set, update } = writable(EMPTY_LIST);
 
+	const toggleItemInArray = (items: TodoItem[], caption: string): TodoItem[] => {
+		return items.map((item) => {
+			if (item.caption === caption) {
+				return {
+					...item,
+					completed: !item.completed
+				};
+			} else {
+				return item;
+			}
+		});
+	};
+
+	const toggleItem = (caption: string) => {
+		update((todoList) => {
+			return {
+				...todoList,
+				items: toggleItemInArray(todoList.items, caption)
+			};
+		});
+	};
+
 	return {
 		subscribe,
 		set,
-		toggleItem: (caption: string) => {
-			update((todoList) => {
-				return {
-					...todoList,
-					items: _toggleItem(todoList.items, caption)
-				};
-			});
-		}
+		toggleItem
 	};
 })();
-export const todoCountStore: Readable<number> = derived(todoStore, ($todoStore) => {
-	const l = $todoStore.items.filter((item) => !item.completed).length;
-	console.log(l);
-	return l;
-});
+
+export const todoCountStore: Readable<number> = derived(
+	todoStore,
+	($todoStore) => $todoStore.items.filter((item) => !item.completed).length
+);
