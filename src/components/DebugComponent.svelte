73fd55543ea8194c2e3c2d4ba7b2aab0c4c88427
sebastian-mrozek <sebastian.mrozek@gmail.com>
2021-04-27 13:24:22 +1200

    Add debug component

---
+++ src/components/DebugComponent.svelte
@@ -0,0 +1,23 @@
+<script lang="typescript">
+	import { todoStore } from '../data/todoStore';
+</script>
+
+<div class="container">
+	All tasks:
+	<div>
+		{#each $todoStore.items as item}
+			{item.id}: '{item.caption}' is {!item.completed ? 'not' : ''} done<br />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.container {
+		font-family: monospace;
+		color: gray;
+		background-color: lightgrey;
+		padding: 1em;
+		margin: 2em;
+		border: 1px solid red;
+	}
+</style>
